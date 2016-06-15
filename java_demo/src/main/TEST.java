24b6960de1f94dae0e8d1730dd6dbde230473190
1499046832@qq.com <gityao521314>
2016-06-15 09:29:53 +0800

    java_demo

---
+++ java_demo/src/main/TEST.java
@@ -0,0 +1,21 @@
+package main;
+
+public class TEST {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		   System.out.println("凑整:Math.ceil(2)=" + (int)Math.ceil(2)); 
+		   System.out.println("凑整:Math.ceil(2.1)=" + (int)Math.ceil(2.1)); 
+		   System.out.println("凑整:Math.ceil(2.5)=" + (int)Math.ceil(2.5)); 
+		   System.out.println("凑整:Math.ceil(2.9)=" + (int)Math.ceil(2.9));
+		   
+		   String string = "string";
+		   String s = null;
+		   
+		   System.out.println(s.equals(string));
+	}
+}
