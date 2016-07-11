ad8a48f13f664360127f67fb2225d08271a1d172
1499046832@qq.com <gityao521314>
2016-07-11 14:07:52 +0800

    07-11

--- java_demo/src/main/Test1.java
+++ java_demo/src/main/Test1.java
@@ -12,13 +12,19 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		List<String> list = new ArrayList<String>();
+		list.add("index++");
 		
+		List<String> list2 = new ArrayList<String>();
 		for (int i = 0; i < 10; i++) {
-			list.add("data"+i);
+			list2.add("data"+i);
 		}
-        list.add(5, "hehe");
-        System.out.println(list);
+		list = list2;
 		
+		List<String> list3 = new ArrayList<String>();
+		for (int i = 10; i < 20; i++) {
+			list3.add("data"+i);
+		}
+		list = list3;
+        System.out.println(list);
 	}
-
 }
