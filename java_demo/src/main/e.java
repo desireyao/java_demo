d579196f897dc317d69150a077b771234362778d
1499046832@qq.com <gityao521314>
2016-07-12 10:07:51 +0800

    String 相关

---
+++ java_demo/src/main/e.java
@@ -0,0 +1,20 @@
+package main;
+
+/**
+ * 测试Stirng
+ * @author yaoh
+ *
+ */
+
+public class e {
+	
+	public static void main(String[] args) {
+		
+		int a = 19;
+		System.out.println(String.valueOf(a));
+		
+		String b = null;
+		System.out.println("".equals(b));
+	}
+
+}
