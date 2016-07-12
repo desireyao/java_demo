941b557cb7a4bcd79a2ed35730072bc301c7bbbe
1499046832@qq.com <gityao521314>
2016-07-12 16:05:33 +0800

    f,g

---
+++ java_demo/src/main/f.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.HashMap;
+
+public class f {
+	
+	public static void main(String[] args) {
+		
+		HashMap<String, String> maps = new HashMap<String, String>();
+		System.out.println(maps.get("kk"));
+ 		
+	}
+
+}
---
+++ java_demo/src/main/g.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.io.File;
+
+public class g {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+        File file = new File("asserts/test.tmp");
+        System.out.println(file.length());
+	}
+
+}
