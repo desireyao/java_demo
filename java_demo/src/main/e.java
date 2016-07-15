c86982b373e863025b54a42092a619185d2097f8
1499046832@qq.com <gityao521314>
2016-07-15 09:45:47 +0800

    增加 substring 用法

--- java_demo/src/main/e.java
+++ java_demo/src/main/e.java
@@ -12,9 +12,15 @@ public class e {
 		
 		int a = 19;
 		System.out.println(String.valueOf(a));
-		
 		String b = null;
 		System.out.println("".equals(b));
+		//------------------------------------------
+		
+		String name = "abc.apk";
+		String apkName = name.substring(0, name.length() - 4);
+		System.out.println(apkName);
+		
+		
 	}
 
 }
