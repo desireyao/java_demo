b0d793d0b6b6adda2ad4e32b103bac65c60b07a5
1499046832@qq.com <gityao521314>
2016-08-15 11:22:32 +0800

    test Arraylist LinkedList

--- java_demo/src/main/a.java
+++ java_demo/src/main/a.java
@@ -1,10 +1,36 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class a {
 
+	
 	public static void main(String[] args) {
 		
-		
+		List<Integer> list = new ArrayList<Integer>();
+	    List<Integer> linkList = new LinkedList<Integer>();
+	    
+	   
+//	    long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 5000000; i++) {
+        	list.add(i);
+		 }	 
+	    long startTime = System.currentTimeMillis();
+//	    list.get(499999);
+	    list.remove(49999);
+	    
+        System.out.println("arraylist 所需时间: " + (System.currentTimeMillis() - startTime));
+	    
+//        startTime = System.currentTimeMillis();
+	    for (int i = 0; i < 5000000; i++) {
+	    	linkList.add(i);
+		}
+	    startTime = System.currentTimeMillis();
+//	    linkList.get(499999);
+	    linkList.remove(49999);
+        System.out.println("linkedlist 所需时间: " + (System.currentTimeMillis() - startTime));
 	}
 
 }
